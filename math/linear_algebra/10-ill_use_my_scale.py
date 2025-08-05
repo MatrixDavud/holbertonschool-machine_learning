55db8398d4448b0a6f3758a876963b928908b618
Davud <11396@holbertonstudents.com>
2025-08-05 23:12:51 +0400

    10th task v3

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -4,10 +4,4 @@
 
 def np_shape(matrix):
     """Return the shape of matrix."""
-    shape = []
-    shape.append(len(matrix))
-    check_list = matrix[0]
-    while type(check_list) is list:
-        shape.append(len(check_list))
-        check_list = check_list[0]
-    return tuple(shape)
+    return matrix.shape
