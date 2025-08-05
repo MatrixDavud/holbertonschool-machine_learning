c0d556a95edb632b9e6d3b10023282eaa52aabab
Davud <11396@holbertonstudents.com>
2025-08-05 23:27:47 +0400

    13th task v2

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,7 @@
 """Perform operations on ndarray."""
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
     """Concatenate two matrices along the given axis."""
     return np.concatenate((mat1, mat2), axis)
