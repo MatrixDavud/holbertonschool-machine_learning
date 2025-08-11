73985742e026aaba859fa6cc9b52bc64a38de8ee
Davud <11396@holbertonstudents.com>
2025-08-11 15:58:46 +0400

    9th task calculus v3

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """Return the summation of i^2 till n, where n should be valid int."""
-    if type(n) is not int:
+    if type(n) is not int or n <= 0:
         return None
     else:
         return int((n * (n+1) * (2*n+1))/6)
