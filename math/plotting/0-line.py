9fd2dd700e474f51e453f41a5aabb21932384336
Davud <11396@holbertonstudents.com>
2025-08-11 13:14:25 +0400

    0th task plotting v2

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Plotting."""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
 
+def line():
+    """Plot a line where x axis is numbers 0-10 and y is x**3."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
