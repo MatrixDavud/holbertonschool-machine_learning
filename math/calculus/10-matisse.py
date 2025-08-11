e11bb998d13d398e80c122af6e872626d399d55a
Davud <11396@holbertonstudents.com>
2025-08-11 16:29:27 +0400

    10th task calculus v4

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,16 +4,16 @@
 
 def poly_derivative(poly):
     """Return the list of coefs of the dy of the polynomial."""
-    if type(poly) is not list or\
-            not all(isinstance(coef, (int, float)) for coef in poly):
+    if type(poly) is not list or not \
+            all(isinstance(coef, (int, float)) for coef in poly):
         return None
-    else:
-        if len(poly) == 1:
-            return None
-        else:
-            if all(coef == 0 for coef in poly):
-                return None
-            dy_list = []
-            for i in range(1, len(poly)):
-                dy_list.append(poly[i]*i)
-            return dy_list
+
+    if len(poly) == 1:
+        return [0]
+
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+
+    if all(coef == 0 for coef in derivative):
+        return [0]
+
+    return derivative
