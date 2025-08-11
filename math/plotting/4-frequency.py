11ef8bbd30f360b7481ba6de255fe38415dd50c7
Davud <11396@holbertonstudents.com>
2025-08-11 14:38:38 +0400

    4th task plotting

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Bar graph plotting."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Draw a bar graph of grade frequencies."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=np.arange(0, 110, 10), edgecolor='black')
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.xlim(0, 100)
+    plt.ylabel('Number of Students')
+    plt.show()
