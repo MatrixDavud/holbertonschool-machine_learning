dc596960dd6921529c9e1346ffb54227721589eb
Davud <11396@holbertonstudents.com>
2025-08-11 16:16:26 +0400

    10th task calculus

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Calculus operations."""
+
+
+def poly_derivative(poly):
+    """Return the list of coefs of the dy of the polynomial."""
+    if type(poly) is not list or\
+            not all(isinstance(coef, (int, float)) for coef in poly):
+        return None
+    else:
+        if len(poly) == 1:
+            dy = [0]
+            return dy
+        else:
+            dy_list = []
+            for i in range(1, len(poly)):
+                dy_list.append(poly[i]*i)
+            return dy_list
