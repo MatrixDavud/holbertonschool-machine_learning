58cbb3e1f77f231afcd85cbc6e44e8240b9ae4b8
Davud <11396@holbertonstudents.com>
2025-08-11 17:01:47 +0400

    17th task calculus

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Performing Calculus."""
+
+
+def poly_integral(poly, C=0):
+    """Return the list of coefs of the anti-dy of polynomial."""
+    if type(poly) is not list:
+        return None
+
+    if not isinstance(C, int):
+        return None
+
+    for coef in poly:
+        if not isinstance(coef, (int, float)):
+            return None
+
+    coefs = []
+    coefs.append(C)
+    for i in range(len(poly)):
+        coefs.append(poly[i] / (i+1))
+    coefs = [int(c) if isinstance(c, float) and
+             c.is_integer() else c for c in coefs]
+
+    while len(coefs) > 1 and coefs[-1] == 0:
+        coefs.pop()
+    return coefs
