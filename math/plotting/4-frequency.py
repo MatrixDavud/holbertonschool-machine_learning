5e2ca4e7754cf1670c816fde62bbce0a87ebc19a
MatrixDavud <qdavud395@gmail.com>
2025-08-12 17:37:13 +0400

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,36 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def bars():
-    """Create a bar graph representing number of fruit per person."""
+def frequency():
+    """Draw a bar graph of grade frequencies."""
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4, 3))
-
+    student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    people = ['Farrah', 'Fred', 'Felicia']
-    x = np.arange(len(people))
-
-    colors = {
-        'apples': 'red',
-        'bananas': 'yellow',
-        'oranges': '#ff8000',
-        'peaches': '#ffe5b4'
-    }
-
-    bottom = np.zeros(len(people))
-
-    fruits = ['apples', 'bananas', 'oranges', 'peaches']
-    for i, fruit_name in enumerate(fruits):
-        plt.bar(x, fruit[i], width=0.5, bottom=bottom,
-                color=colors[fruit_name], label=fruit_name)
-        bottom += fruit[i]
-
-    plt.xticks(x, people)
-    plt.ylabel('Quantity of Fruit')
-    plt.ylim(0, 80)
-    plt.yticks(range(0, 81, 10))
-    plt.title('Number of Fruit per Person')
-    plt.legend()
-
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.axis((0, 100, 0, 30))
+    plt.title('Project A')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.xticks(bins)
     plt.show()
