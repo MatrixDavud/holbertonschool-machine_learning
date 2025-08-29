24cbbb2166085b43fff0f156867eb7f346ccc911
Davud <11396@holbertonstudents.com>
2025-08-30 01:53:19 +0400

    16th task classification

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Classification algorithm using Deep Neural Network (DNN class)."""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Deep Neural Network class."""
+
+    def __init__(self, nx, layers):
+        """Construct the deep neural network object."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0\
+                or not all(isinstance(x, int) and x > 0 for x in layers):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for l in range(1, self.L + 1):
+            # number of nodes in current layer
+            nodes = layers[l - 1]
+
+            # number of nodes in the previous layer (nx for first layer)
+            prev_nodes = nx if l == 1 else layers[l - 2]
+
+            # He initialization for weights
+            self.weights["W{}".format(l)] = (
+                np.random.randn(nodes, prev_nodes) * np.sqrt(2 / prev_nodes)
+            )
+
+            # Bias initialized as zeros
+            self.weights["b{}".format(l)] = np.zeros((nodes, 1))
