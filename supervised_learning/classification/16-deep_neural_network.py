7ee31145553febb0c5b5d5712d45204dd20237bf
Davud <11396@holbertonstudents.com>
2025-08-30 02:05:55 +0400

    17th task classification v2

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -12,8 +12,10 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0\
-                or not all(isinstance(x, int) and x > 0 for x in layers):
+
+        layers_arr = np.array(layers)
+
+        if not np.issubdtype(layers_arr.dtype, np.integer) or np.any(layers_arr <= 0):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
