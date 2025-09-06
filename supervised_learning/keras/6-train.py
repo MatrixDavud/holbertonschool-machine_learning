89829c9223f4e9e810af27f1b4cf983d5dc70e05
Davud <11396@holbertonstudents.com>
2025-09-06 16:25:56 +0400

    6th task Keras v2

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -4,8 +4,8 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
-                 validation_data=None, early_stopping=False, patience=0,
-                   verbose=True, shuffle=False):
+                validation_data=None, early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
     """Train a model using mini-batch gradient descent."""
     callbacks = []
 
