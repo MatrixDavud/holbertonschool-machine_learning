0859afccbcdb87b5db3462be547fc7cad7beaccf
Davud <11396@holbertonstudents.com>
2025-09-21 21:18:07 +0400

    1st task Regularization v2

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -11,10 +11,10 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     A_L = cache[A_prev_key]
     dZ = A_L - Y
 
-    for l in range(L, 0, -1):
-        W_key = "W{}".format(l)
-        b_key = "b{}".format(l)
-        A_prev_key = "A{}".format(l - 1)
+    for i in range(L, 0, -1):
+        W_key = "W{}".format(i)
+        b_key = "b{}".format(i)
+        A_prev_key = "A{}".format(i - 1)
 
         Wl = weights[W_key]
         bl = weights[b_key]
@@ -23,12 +23,12 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         dW = (1.0 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * Wl
         db = (1.0 / m) * np.sum(dZ, axis=1, keepdims=True)
 
-        if l > 1:
+        if i > 1:
             dA_prev = np.dot(Wl.T, dZ)
 
         weights[W_key] = Wl - alpha * dW
         weights[b_key] = bl - alpha * db
 
-        if l > 1:
+        if i > 1:
             A_prev_activation = A_prev
             dZ = dA_prev * (1 - A_prev_activation ** 2)
