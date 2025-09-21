c05a5c48ca86f8b5308e52f649edcaa777a785a2
Davud <11396@holbertonstudents.com>
2025-09-21 22:04:27 +0400

    5th task Regularization v2

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -6,26 +6,26 @@ import numpy as np
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """Update the weights of a NN with Dropout regularization using GD."""
     m = Y.shape[1]
-    dZ = {}
+    dZ = cache['A' + str(L)] - Y
 
-    for i in reversed(range(1, L + 1)):
-        A = cache['A' + str(i)]
-        A_prev = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
-
-        if i == L:
-            dZ[i] = A - Y
+    for i in range(L, 0, -1):
+        if i == 1:
+            A_prev = cache['A0']
         else:
-            dA = np.dot(weights['W' + str(i + 1)].T, dZ[i + 1])
+            A_prev = cache['A' + str(i - 1)]
 
-            D = cache['D' + str(i)]
-            dA *= D
-            dA /= keep_prob
-
-            dZ[i] = dA * (1 - A ** 2)
-
-        dW = np.dot(dZ[i], A_prev.T) / m
-        db = np.sum(dZ[i], axis=1, keepdims=True) / m
+        dW = (1 / m) * np.dot(dZ, A_prev.T)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
         weights['W' + str(i)] -= alpha * dW
         weights['b' + str(i)] -= alpha * db
+
+        if i > 1:
+            dA_prev = np.dot(weights['W' + str(i)].T, dZ)
+
+            dA_prev *= cache['D' + str(i - 1)]
+            dA_prev /= keep_prob
+
+
+            A_prev_layer = cache['A' + str(i - 1)]
+            dZ = dA_prev * (1 - A_prev_layer ** 2)
