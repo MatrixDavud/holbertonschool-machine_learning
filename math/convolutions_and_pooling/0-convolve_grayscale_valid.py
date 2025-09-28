df16dbfeff542085e383dd63278ec2b864780eea
Davud <11396@holbertonstudents.com>
2025-09-28 16:26:40 +0400

    0th task Convolution and Pooling v2

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,15 +10,8 @@ def convolve_grayscale_valid(images, kernel):
     conv_h = h-kh+1
     conv_w = w-kw+1
     convolved_images = np.zeros((m, conv_h, conv_w))
-    for i in range(m):
-        for k in range(conv_h*conv_w):
-            row = int(k / conv_w)
-            col = k % conv_w
-
-            mat_i = images[i, row:row+kh, col:col+kw]
-            res = mat_i * kernel
-            s_elems = np.sum(res)
-
-            convolved_images[i, row, col] = s_elems
-
+    for i in range(conv_h):
+        for k in range(conv_w):
+            convolved_images[:, i, k] = np.sum(images[:, i:i+kh, k:k+kw]
+                                               * kernel, axis=(1, 2))
     return convolved_images
