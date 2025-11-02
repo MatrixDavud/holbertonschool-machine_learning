b343e7751a5259c31de3c6bca2d7a88df357dbd6
Davud <11396@holbertonstudents.com>
2025-11-02 22:42:58 +0400

    Neural Style Transfer 4th task

--- supervised_learning/neural_style_transfer/4-neural_style.py
+++ supervised_learning/neural_style_transfer/4-neural_style.py
@@ -106,3 +106,20 @@ class NST:
                                     for style_feature in style_outputs[:-1]]
 
         self.content_feature = self.model(content_image)[-1]
+
+    def layer_style_cost(self, style_output, gram_target):
+        """Calculate the style cost for a single layer."""
+        if not isinstance(style_output, (tf.Tensor, tf.Variable)):
+            raise TypeError("style_output must be a tensor of rank 4")
+        if len(style_output.shape) != 4:
+            raise TypeError("style_output must be a tensor of rank 4")
+        ch = style_output.shape[-1]
+        if not isinstance(gram_target, (tf.Tensor, tf.Variable)):
+            raise TypeError(
+                f"gram_target must be a tensor of shape [1, {ch}, {ch}]")
+        if len(gram_target.shape) != 3 or gram_target.shape != [1, ch, ch]:
+            raise TypeError(
+                f"gram_target must be a tensor of shape [1, {ch}, {ch}]")
+
+        gram_style = self.gram_matrix(style_output)
+        return tf.reduce_mean(tf.square(gram_style - gram_target))
