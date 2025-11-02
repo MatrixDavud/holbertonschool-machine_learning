a02d4634d33bae3fda6f76892f165f8c223a302f
Davud <11396@holbertonstudents.com>
2025-11-02 18:06:44 +0400

    Object detection 5th task v3

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -170,8 +170,8 @@ class Yolo:
         pimages = []
         image_shapes = []
 
-        input_h = self.model.input.shape[1].value
-        input_w = self.model.input.shape[2].value
+        input_h = self.model.input.shape[1]
+        input_w = self.model.input.shape[2]
 
         for image in images:
             image_shapes.append([image.shape[0], image.shape[1]])
