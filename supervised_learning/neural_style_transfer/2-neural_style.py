b3520001f85356e696e94c04dd1556a587fbab3d
Davud <11396@holbertonstudents.com>
2025-11-02 22:09:23 +0400

    Neural Style Transfer 2nd task v2

--- supervised_learning/neural_style_transfer/2-neural_style.py
+++ supervised_learning/neural_style_transfer/2-neural_style.py
@@ -98,11 +98,11 @@ class NST:
     @staticmethod
     def gram_matrix(input_layer):
         """Calculate the Gram matrix of a given layer output."""
-        if not isinstance(input_layer,
-                         (tf.Tensor, tf.Variable)) or input_layer.ndim != 4:
+        if not isinstance(input_layer, (tf.Tensor, tf.Variable)):
             raise TypeError("input_layer must be a tensor of rank 4")
-        _, h, w, c = input_layer.shape
-        features = tf.reshape(input_layer, (h*w, c))
-        gram = tf.linalg.matmul(features, features, transpose_a=True)
+        if len(input_layer.shape) != 4:
+            raise TypeError("input_layer must be a tensor of rank 4")
+        input_layer = tf.reshape(input_layer, [-1, input_layer.shape[-1]])
+        gram = tf.matmul(input_layer, input_layer, transpose_a=True)
         gram = tf.expand_dims(gram, axis=0)
-        return gram
+        return gram / tf.cast(tf.shape(input_layer)[0], tf.float32)
