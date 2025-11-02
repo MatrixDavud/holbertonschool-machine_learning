695bb3c3224313277d524fdd06e31df226015b7f
Davud <11396@holbertonstudents.com>
2025-11-02 21:43:19 +0400

    Neural Style Transfer 1st task v6

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -3,8 +3,9 @@
 import numpy as np
 import tensorflow as tf
 
+
 class NST:
-    """Neural Style Transfer class"""
+    """Neural Style Transfer class."""
 
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
@@ -35,12 +36,19 @@ class NST:
         self.alpha = alpha
         self.beta = beta
 
-        # Load the model immediately
         self.model = self.load_model()
 
     @staticmethod
     def scale_image(image):
-        """Rescale an image so pixels are in [0, 1] and max side is 512px."""
+        """
+        Rescale an image so pixels are in [0, 1] and max side is 512px.
+
+        Args:
+            image: numpy.ndarray with shape (h, w, 3)
+
+        Returns:
+            tf.Tensor with shape (1, h_new, w_new, 3)
+        """
         if not isinstance(image, np.ndarray) or \
            image.ndim != 3 or image.shape[2] != 3:
             raise TypeError(
@@ -48,6 +56,7 @@ class NST:
             )
 
         h, w = image.shape[:2]
+
         if h > w:
             h_new = 512
             w_new = int(w * 512 / h)
@@ -60,18 +69,26 @@ class NST:
             size=[h_new, w_new],
             method=tf.image.ResizeMethod.BICUBIC
         )
-        image_rescaled = tf.clip_by_value(image_resized / 255.0, 0.0, 1.0)
-        return tf.expand_dims(image_rescaled, axis=0)
+
+        image_rescaled = image_resized / 255.0
+
+        image_rescaled = tf.clip_by_value(image_rescaled, 0.0, 1.0)
+
+        image_final = tf.expand_dims(image_rescaled, axis=0)
+
+        return image_final
 
     def load_model(self):
         """
-        Creates the VGG19 model used to calculate content and style features.
+        Create the VGG19 model used to calculate content and style features.
+
         Outputs the style layers followed by the content layer.
         """
-        vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+        vgg = tf.keras.applications.VGG19(include_top=False,
+                                          weights='imagenet')
         vgg.trainable = False
 
         outputs = [vgg.get_layer(name).output for name in self.style_layers]
         outputs.append(vgg.get_layer(self.content_layer).output)
 
-        return tf.keras.models.Model(inputs=vgg.input, outputs=outputs)
+        return tf.keras.Model(inputs=vgg.input, outputs=outputs)
