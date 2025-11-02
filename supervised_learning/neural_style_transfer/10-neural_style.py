8b2c75383e28e62586b1ad90d652338e0dda561c
Davud <11396@holbertonstudents.com>
2025-11-02 23:43:11 +0400

    Neural Style Transfer 10th task v3

--- supervised_learning/neural_style_transfer/10-neural_style.py
+++ supervised_learning/neural_style_transfer/10-neural_style.py
@@ -261,7 +261,7 @@ content {J_content.numpy()}, style {J_style.numpy()}")
         if J < best_cost:
             best_cost = J
             best_image = generated_image.numpy()[0]
-        if step is not None and i % step == 0:
+        if step is not None:
             print(f"Cost at iteration {iterations}: {J.numpy()}, \
 content {J_content.numpy()}, style {J_style.numpy()}")
         return best_image, best_cost.numpy()
