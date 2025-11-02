f9fc2812d668e0955b5d68e8a895102b6a66fcb8
Davud <11396@holbertonstudents.com>
2025-11-02 22:32:11 +0400

    Neural Style Transfer 3rd task v6

--- supervised_learning/neural_style_transfer/3-neural_style.py
+++ supervised_learning/neural_style_transfer/3-neural_style.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-"""Neural Style Transfer Implementation."""
+"""
+3-neural_style.py
+"""
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """Neural Style Transfer class."""
-
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
-                    'block4_conv1', 'block5_conv1']
+    """
+    class to perform neural style transfer
+    """
+    style_layers = [
+        'block1_conv1', 'block2_conv1', 'block3_conv1',
+        'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        """Initialize class object."""
+        """
+        class constructor
+        """
         valid = (isinstance(style_image, np.ndarray)
                  and style_image.ndim == 3 and style_image.shape[2] == 3)
         if not valid:
@@ -38,84 +44,84 @@ class NST:
     @staticmethod
     def scale_image(image):
         """
-        Rescale an image so pixels are in [0, 1] and max side is 512px.
-
-        Args:
-            image: numpy.ndarray with shape (h, w, 3)
-
-        Returns:
-            tf.Tensor with shape (1, h_new, w_new, 3)
+        scales an image such that its
         """
-        if not isinstance(image, np.ndarray) or \
-           image.ndim != 3 or image.shape[2] != 3:
+        valid = (isinstance(image, np.ndarray)
+                 and image.ndim == 3 and image.shape[2] == 3)
+        if not valid:
             raise TypeError(
-                "image must be a numpy.ndarray with shape (h, w, 3)"
-            )
+                "image must be a numpy.ndarray with shape (h, w, 3)")
 
-        h, w = image.shape[:2]
+        h, w, _ = image.shape
+        max_dim = max(h, w)
+        scale = 512 / max_dim
+        new_size = (int(h * scale), int(w * scale))
+        # Convert to tensor and add batch dimension
+        image_tensor = tf.convert_to_tensor(image)[tf.newaxis, ...]
 
-        if h > w:
-            h_new = 512
-            w_new = int(w * 512 / h)
-        else:
-            w_new = 512
-            h_new = int(h * 512 / w)
-
-        image_resized = tf.image.resize(
-            image,
-            size=[h_new, w_new],
+        # Resize using bicubic interpolation (while in [0, 255] range)
+        resized_image = tf.image.resize(
+            image_tensor,
+            new_size,
             method=tf.image.ResizeMethod.BICUBIC
         )
 
-        image_rescaled = image_resized / 255.0
+        # rescale to [0, 1]
+        scaled_image = tf.clip_by_value(resized_image / 255.0, 0.0, 1.0)
 
-        image_rescaled = tf.clip_by_value(image_rescaled, 0.0, 1.0)
-
-        image_final = tf.expand_dims(image_rescaled, axis=0)
-
-        return image_final
+        return scaled_image
 
     def load_model(self):
-        """Create the model used to calculate cost."""
-        vgg = tf.keras.applications.VGG19(
-            include_top=False,
-            weights='imagenet',
-        )
+        """
+        creates the model used to calculate cost
+        """
+        # Load VGG19 without top layers and pretrained on ImageNet
+        vgg = tf.keras.applications.VGG19(include_top=False,
+                                          weights='imagenet')
+        # Replace MaxPooling2D layers with AveragePooling2D
+        pooling_layers = {"MaxPooling2D": tf.keras.layers.AveragePooling2D}
+        vgg.save("base_vgg")
+        # Reload the VGG model with the pooling layers swapped
+        vgg = tf.keras.models.load_model("base_vgg",
+                                         custom_objects=pooling_layers)
+        # Make sure that the model is non-trainable
         vgg.trainable = False
-
-        for layer in vgg.layers:
-            if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                layer.__class__ = tf.keras.layers.AveragePooling2D
-
-        style_outputs = [
-            vgg.get_layer(name).output for name in self.style_layers]
-        content_output = vgg.get_layer(self.content_layer).output
-
-        self.model = tf.keras.Model(
-            inputs=vgg.input,
-            outputs=[content_output] + style_outputs
-        )
+        # Get the desired layer outputs
+        outputs = [vgg.get_layer(layer).output for layer in self.style_layers]
+        outputs.append(vgg.get_layer(self.content_layer).output)
+        # Create a model that returns the outputs
+        self.model = tf.keras.models.Model(inputs=vgg.input, outputs=outputs)
 
     @staticmethod
     def gram_matrix(input_layer):
-        """Calculate the Gram matrix of a given layer output."""
+        """
+        calculate gram matrices
+        """
         if not isinstance(input_layer, (tf.Tensor, tf.Variable)):
             raise TypeError("input_layer must be a tensor of rank 4")
         if len(input_layer.shape) != 4:
             raise TypeError("input_layer must be a tensor of rank 4")
+        # Unroll the input layer
         input_layer = tf.reshape(input_layer, [-1, input_layer.shape[-1]])
+        # Calculate the Gram matrix
         gram = tf.matmul(input_layer, input_layer, transpose_a=True)
-        gram = tf.expand_dims(gram, axis=0)
+        # Normalize by the number of elements in the batch
+        gram = tf.expand_dims(gram, axis=0)  # Add batch dimension
         return gram / tf.cast(tf.shape(input_layer)[0], tf.float32)
 
     def generate_features(self):
-        """Extract the features used to calculate neural style cost."""
+        """
+        Extracts the features used to calculate neural style cost
+        """
         content_image = tf.keras.applications.vgg19.preprocess_input(
             self.content_image * 255)
         style_image = tf.keras.applications.vgg19.preprocess_input(
             self.style_image * 255)
+        # Get the style features and content features
+        # For style image outputs
         style_outputs = self.model(style_image)
         self.gram_style_features = [self.gram_matrix(style_feature)
                                     for style_feature in style_outputs[:-1]]
 
+        # For content image outputs
         self.content_feature = self.model(content_image)[-1]
