e384fa437f0965b09cac90a1664ae0b700f0e6e7
Davud <11396@holbertonstudents.com>
2025-11-11 15:18:39 +0400

    8th task probability

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Normal Distribution."""
+pi = 3.1415926536
+e = 2.7182818285
 
 
 class Normal:
@@ -32,3 +34,9 @@ class Normal:
     def x_value(self, z):
         """Calculate the x-value of a given z-score."""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculate the value of the PDF for a given x-value."""
+        factor = 1 / (self.stddev * (2*pi)**0.5)
+        exponent = e ** (-0.5*(self.z_score(x))**2)
+        return factor * exponent
