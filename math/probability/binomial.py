0e6abb473246f8a09fcb20c8e7784026c2ee31fd
Davud <11396@holbertonstudents.com>
2025-11-11 15:59:47 +0400

    11th task probability v2

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -48,6 +48,6 @@ class Binomial:
     
     def pmf(self, k):
         """Calculate the value of the PMF for a given number of successes."""
-        factor_1 = self.n_choose_k(self.n, k)
+        factor_1 = self.n_choose_k(self.n, int(k))
         factor_2 = self.p**k * (1-self.p)**(self.n - k)
         return factor_1 * factor_2
