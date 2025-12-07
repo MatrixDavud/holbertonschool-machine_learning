30ae1106ba47b0e9198ad77f6fe2c0497c740007
Davud <11396@holbertonstudents.com>
2025-12-07 21:25:25 +0400

    3rd task Clustering v4

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -22,11 +22,23 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     kmeans = __import__('1-kmeans').kmeans
     variance = __import__('2-variance').variance
 
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
     if kmax is None:
-        kmax = X.shape[0]
-    if kmax - kmin < 1:
+        return None, None
+    if not isinstance(kmax, int) or kmax <= 0:
+        return None, None
+    if kmin > kmax:
+        return None, None
+    if kmax > X.shape[0]:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
+    if kmax - kmin < 1:
+        return None, None
     results = []
     variances = []
 
