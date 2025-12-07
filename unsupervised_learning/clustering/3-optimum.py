7be2e2bfefb241fd08af5bc7920e81efbbbccb61
Davud <11396@holbertonstudents.com>
2025-12-07 21:31:46 +0400

    3rd task Clustering v5

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -4,53 +4,62 @@ import numpy as np
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """
-    Tests for the optimum number of clusters by variance.
+    """Test for the optimum number of clusters by variance.
 
     Args:
-        X (numpy.ndarray): Data set of shape (n, d)
-        kmin (int): Minimum number of clusters to evaluate
-        kmax (int): Maximum number of clusters to evaluate
-        iterations (int): Max iterations for K-means
+        X (numpy.ndarray): Dataset of shape (n, d).
+        kmin (int): Minimum number of clusters to check (inclusive).
+        kmax (int): Maximum number of clusters to check (inclusive).
+        iterations (int): Maximum number of iterations for K-means.
 
     Returns:
-        results: list of (C, clss) for each k
-        d_vars: list of variance differences from smallest k
-
-        Or (None, None) on failure.
+        tuple: (results, d_vars) where:
+            - results is a list of K-means outputs for each cluster size
+            - d_vars is a list of variance differences from smallest cluster
+        Returns (None, None) on failure.
     """
     kmeans = __import__('1-kmeans').kmeans
     variance = __import__('2-variance').variance
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    if not isinstance(kmin, int) or kmin <= 0:
+    
+    n, d = X.shape
+
+    if not isinstance(kmin, int) or kmin <= 0 or kmin >= n:
         return None, None
+
     if kmax is None:
+        kmax = n
+    
+    if not isinstance(kmax, int) or kmax <= 0 or kmax > n:
         return None, None
-    if not isinstance(kmax, int) or kmax <= 0:
+
+    if kmin >= kmax:
         return None, None
-    if kmin > kmax:
-        return None, None
-    if kmax > X.shape[0]:
-        return None, None
+
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    if kmax - kmin < 1:
-        return None, None
     results = []
     variances = []
 
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
-        if C is None:
+
+        if C is None or clss is None:
             return None, None
 
         results.append((C, clss))
-        variances.append(variance(X, C))
 
-    base = variances[0]
-    d_vars = [base - v for v in variances]
+        var = variance(X, C)
+
+        if var is None:
+            return None, None
+        
+        variances.append(var)
+
+    baseline_variance = variances[0]
+    d_vars = [baseline_variance - var for var in variances]
 
     return results, d_vars
