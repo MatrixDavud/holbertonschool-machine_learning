d993e06fc621502d0d4fd7d25e6855f51f48fce5
Davud <11396@holbertonstudents.com>
2025-12-21 14:45:03 +0400

    3rd task autoencoders v2

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -8,42 +8,38 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     # Encoder
     encoder_inputs = keras.Input(shape=(input_dims,))
     x = encoder_inputs
-
     for nodes in hidden_layers:
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
     z_mean = keras.layers.Dense(latent_dims, activation=None)(x)
     z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)
 
-    # Sampling function for the reparameterization trick
     def sampling(args):
-        mu, log_var = args
+        """Reparameterization trick: samples z = mu + epsilon * sigma."""
+        mu, log_sig = args
         batch = keras.backend.shape(mu)[0]
-        dim = keras.backend.int_shape(mu)[1]
-        epsilon = keras.backend.random_normal(shape=(batch, dim))
-        return mu + keras.backend.exp(0.5 * log_var) * epsilon
+        dims = keras.backend.int_shape(mu)[1]
+        epsilon = keras.backend.random_normal(shape=(batch, dims))
+        return mu + keras.backend.exp(log_sig / 2) * epsilon
 
-    # Latent space output
     z = keras.layers.Lambda(sampling,
                             output_shape=(latent_dims,))([z_mean, z_log_var])
-
     encoder = keras.Model(encoder_inputs,
                           [z, z_mean, z_log_var], name='encoder')
 
     # Decoder
     decoder_inputs = keras.Input(shape=(latent_dims,))
     x = decoder_inputs
-
     for nodes in reversed(hidden_layers):
         x = keras.layers.Dense(nodes, activation='relu')(x)
 
     decoder_outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
-
     decoder = keras.Model(decoder_inputs, decoder_outputs, name='decoder')
 
-    # --- Full Autoencoder ---
-    auto_outputs = decoder(encoder(encoder_inputs)[0])
-    auto = keras.Model(encoder_inputs, auto_outputs, name='vae')
+    # Full Autoencoder
+    z_sampled = encoder(encoder_inputs)[0]
+    auto_outputs = decoder(z_sampled)
+    auto = keras.Model(encoder_inputs, auto_outputs, name='auto')
 
     auto.compile(optimizer='adam', loss='binary_crossentropy')
 
