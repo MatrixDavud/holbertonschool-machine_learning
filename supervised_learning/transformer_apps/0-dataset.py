9296a09416849c3b87f02ddba3f0b19a6f29bd22
Davud <11396@holbertonstudents.com>
2026-02-15 20:16:46 +0400

    0th task transformer applications v14

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,82 +1,55 @@
 #!/usr/bin/env python3
 """
-Defines class Dataset that loads and preps a dataset for machine translation
+Dataset loading for transformer.
 """
-
-
-import transformers
 import tensorflow_datasets as tfds
+import transformers
+
 
 
 class Dataset:
     """
-    Loads and preps a dataset for machine translation
-
-    class constructor:
-        def __init__(self)
-
-    public instance attributes:
-        data_train:
-            contains the ted_hrlr_translate/pt_to_en
-                tf.data.Dataset train split, loaded as_supervided
-        data_valid:
-            contains the ted_hrlr_translate/pt_to_en
-                tf.data.Dataset validate split, loaded as_supervided
-        tokenizer_pt:
-            the Portuguese tokenizer created from the training set
-        tokenizer_en:
-            the English tokenizer created from the training set
-
-    instance method:
-        def tokenize_dataset(self, data):
-            that creates sub-word tokenizers for our dataset
+    asdsadsada
     """
     def __init__(self):
         """
-        Class constructor
-
-        Sets the public instance attributes:
-            data_train:
-                contains the ted_hrlr_translate/pt_to_en
-                    tf.data.Dataset train split, loaded as_supervided
-            data_valid:
-                contains the ted_hrlr_translate/pt_to_en
-                    tf.data.Dataset validate split, loaded as_supervided
-            tokenizer_pt:
-                the Portuguese tokenizer created from the training set
-            tokenizer_en:
-                the English tokenizer created from the training set
+            Init the object.
         """
-        self.data_train = tfds.load("ted_hrlr_translate/pt_to_en",
-                                    split="train",
-                                    as_supervised=True)
-        self.data_valid = tfds.load("ted_hrlr_translate/pt_to_en",
-                                    split="validation",
-                                    as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+        examples, _ = tfds.load(
+            'ted_hrlr_translate/pt_to_en',
+            as_supervised=True,
+            with_info=True
+        )
+
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
+        a, b = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = a, b
 
     def tokenize_dataset(self, data):
         """
-        Creates sub_word tokenizers for our dataset
-
-        parameters:
-            data [tf.data.Dataset]:
-                dataset to use whose examples are formatted as tuple (pt, en)
-                pt [tf.Tensor]:
-                    contains the Portuguese sentence
-                en [tf.Tensor]:
-                    contains the corresponding English sentence
-        returns:
-            tokenizer_pt, tokenizer_en:
-                tokenizer_pt: the Portuguese tokenizer
-                tokenizer_en: the English tokenizer
+            Tokenize dataset.
         """
-        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
-        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data),
-            target_vocab_size=(2 ** 15))
-        tokenizer_en = SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data),
-            target_vocab_size=(2 ** 15))
-        return tokenizer_pt, tokenizer_en
+
+        abv = "neuralmind/bert-base-portuguese-cased"
+        tpt = transformers.AutoTokenizer.from_pretrained(abv)
+        ten = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
+
+        pt_sentences = []
+        en_sentences = []
+
+        for pt, en in data:
+            pt_sentences.append(pt.numpy().decode('utf-8'))
+            en_sentences.append(en.numpy().decode('utf-8'))
+
+        tpt = tpt.train_new_from_iterator(
+            pt_sentences,
+            vocab_size=2**13
+        )
+
+        ten = ten.train_new_from_iterator(
+            en_sentences,
+            vocab_size=2**13
+        )
+
+        return tpt, ten
